55b17f5ce5a37206c8830babdc7ae70cfbfacfcf
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-16 17:04:04 -0500

    Basic outline and library inclusion.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-minnpost.github.io
-==================
+# minnpost.github.io
 
 A Github page for the MinnPost organization that is focused on giving an overview of our code on Github.
---
+++ js/app.js
@@ -0,0 +1,22 @@
+/**
+ * Main JS for MinnPost.github.io
+ */
+(function($, undefined) {
+  
+  var repoURL = 'https://api.github.com/repos/minnpost/[[[REPO_NAME]]]';
+  var orgURL = 'https://api.github.com/orgs/minnpost';
+  
+  // Specific repos to get with a score (1-10)
+  // and a small blurb on status of project
+  var repos = {
+    'state-legislature': {
+      score: 5,
+      status: 'Being updated for resuse'
+    }
+  };
+  
+  $(document).ready(function() {
+    
+    
+  });
+});
